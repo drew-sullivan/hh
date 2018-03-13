5f3d4344634414d862803b9881d6389abda6550b
dsullivan <drew.sullivan@hyland.com>
2018-03-13 13:03:06 -0400

    Got avatars mostly working. Now moving the logic into the new user form

--- src/app/user-manager/components/main-content/main-content.component.ts
+++ src/app/user-manager/components/main-content/main-content.component.ts
@@ -1,3 +1,4 @@
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { ActivatedRoute } from '@angular/router';
@@ -11,6 +12,7 @@ import { UserService } from '../../services/user.service';
 export class MainContentComponent implements OnInit {
 
   user: User;
+
   constructor(
     private route: ActivatedRoute,
     private service: UserService) { }
