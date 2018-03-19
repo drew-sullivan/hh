ee98a7a26e44346bd2f957a5b4a8c2b55c68aca9
ONBASE\cross <chris.ross@onbase.net>
2018-03-19 11:55:17 -0400

    Loaded data from firebase into user service

--- src/app/user-manager/services/user.service.ts
+++ src/app/user-manager/services/user.service.ts
@@ -4,18 +4,33 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { resolve } from 'q';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class UserService {
 
   private _users: BehaviorSubject<User[]>;
+  private internalUserSubscription: User[];
   private dataStore: {
     users: User[]
   };
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private db: AngularFireDatabase) {
     this.dataStore = { users: [] };
     this._users = new BehaviorSubject<User[]>([]);
+    this._users.subscribe( (newUsers) => {
+      this.internalUserSubscription = newUsers;
+    });
+    this.getUsers('/users').subscribe( (users) => {
+      console.log('USER SERVICE ---- USERS');
+      console.log(users);
+      this._users.next(users);
+    });
+  }
+
+  getUsers(path): Observable<any[]> {
+    console.log(this.db.list(path).valueChanges());
+    return this.db.list(path).valueChanges();
   }
 
   get users(): Observable<User[]> {
@@ -31,8 +46,17 @@ export class UserService {
     });
   }
 
-  userById(id: number) {
+  userById(id: number): User {
+    /*
     return this.dataStore.users.find(x => x.id === +id);
+    */
+
+      const filterUsers: User[] = this.internalUserSubscription.filter( (user) => {
+          return user.id == id;
+      });
+      console.log(id);
+      console.log(filterUsers);
+      return filterUsers[0];
   }
 
   addClap(id: number) {
